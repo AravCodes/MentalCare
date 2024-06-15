c1a7c2a4059ac90f5badd31c9f087cf33c7d05cc
Ormon-huehuehue <armnvrma10@gmail.com>
2024-06-15 16:07:27 +0530

    fixed recent studies section bug

--- Frontend/src/Components/NewsDropdown.jsx
+++ Frontend/src/Components/NewsDropdown.jsx
@@ -12,13 +12,11 @@ const NewsDropdown = ({title,description}) => {
 
   return (
     <section>
-      <div id="dropdown" className = {`grid overflow-hidden transition-all duration-300 ease-out text-slate-gray text-[16px] 
-      ${recentStudiesClicked ? " opacity-100" : " grid-rows-[0fr] opacity-0" }`}>
-        <div className = "p-3  rounded-lg  ">
+        {recentStudiesClicked ? <div className = "p-3  rounded-lg  ">
           <Accordion title = {title}   description = {description}/>
-        </div>
+        </div> : null }
+        
 
-      </div>
     </section>
   )
 }
